98ad10ef0d4e3b2ceb83e640de059e4210f4e4d7
5uhwann <5uhwann27@gmail.com>
2022-07-29 23:39:13 +0900

    init project

---
+++ src/main/java/toy/random_meal/RandomMealApplication.java
@@ -0,0 +1,13 @@
+package toy.random_meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/random_meal/RandomMealApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.random_meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
