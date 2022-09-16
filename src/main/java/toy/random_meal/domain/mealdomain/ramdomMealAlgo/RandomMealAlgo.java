7a86ca473534bf4c4953e10e3adf2240587ddc88
5uhwann <5uhwann27@gmail.com>
2022-09-16 23:01:19 +0900

    Revert "refact: RandomMealAlgo 수정"
    This reverts commit 4aecec79cb428a67cef50b2344d5a3e7d9feb4ce.

--- src/main/java/toy/random_meal/domain/mealdomain/ramdomMealAlgo/RandomMealAlgo.java
+++ src/main/java/toy/random_meal/domain/mealdomain/ramdomMealAlgo/RandomMealAlgo.java
@@ -1,8 +1,7 @@
 package toy.random_meal.domain.mealdomain.ramdomMealAlgo;
 
-import toy.random_meal.domain.mealdomain.meal.Meal;
 import toy.random_meal.domain.memberdomain.member.Member;
 
 public interface RandomMealAlgo {
-    Meal randomMealAlgo();
+    String randomMealAlgo(Member member);
 }
