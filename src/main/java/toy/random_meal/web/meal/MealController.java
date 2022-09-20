54a7a0a866ed37345b027ba40da04559e753386c
5uhwann <5uhwann27@gmail.com>
2022-09-20 14:16:00 +0900

    feat: MealController - 음식상세 페이지 매핑

--- src/main/java/toy/random_meal/web/meal/MealController.java
+++ src/main/java/toy/random_meal/web/meal/MealController.java
@@ -4,21 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import toy.random_meal.domain.mealdomain.meal.Meal;
 import toy.random_meal.domain.mealdomain.meal.MealRepository;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/meal")
 @RequiredArgsConstructor
 public class MealController {
 
     private final MealRepository mealRepository;
 
-    @GetMapping("/meal/meals")
+    @GetMapping("/meals")
     public String meals(Model model) {
         List<Meal> meals = mealRepository.findAll();
         model.addAttribute("meals", meals);
         return "/meal/meals";
+    }
+
+    @GetMapping("{mealId}")
+    public String meal(@PathVariable long mealId, Model model) {
+        Meal meal = mealRepository.findById(mealId);
+        model.addAttribute("meal", meal);
+        return "/meal/meal";
     }
 }
