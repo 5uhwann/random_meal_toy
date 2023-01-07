3754bd423eeb2a3b46c2a7d276e22364c63d12b7
5uhwann <5uhwann27@gmail.com>
2023-01-08 00:28:15 +0900

    test: Member 엔티티 생성 메서드 테스트 작성

---
+++ src/test/java/toy/random_meal/domain/member/entity/MemberTest.java
@@ -0,0 +1,22 @@
+package toy.random_meal.domain.member.entity;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    @Test
+    void 회원_생성() {
+        //given
+        String memberName = "5uhwann";
+        Grade grade = Grade.Basic;
+
+        //when
+        Member createdMember = Member.createMember(memberName, grade);
+
+        //then
+        assertThat(createdMember.getName()).isEqualTo(memberName);
+        assertThat(createdMember.getGrade()).isEqualTo(grade);
+    }
+}
