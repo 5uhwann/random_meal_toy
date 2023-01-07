179a80b9a8eec0a88b2102235c28fc7d18d19017
5uhwann <5uhwann27@gmail.com>
2023-01-08 00:29:54 +0900

    feat: Member 엔티티 생성메서드 구현

--- src/main/java/toy/random_meal/domain/member/entity/Member.java
+++ src/main/java/toy/random_meal/domain/member/entity/Member.java
@@ -1,8 +1,6 @@
 package toy.random_meal.domain.member.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,17 +8,18 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import lombok.AccessLevel;
 import lombok.Getter;
-import toy.random_meal.domain.meal.entity.MealList;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "가member_id")
+    @Column(name = "member_id")
     private Long id;
 
     private LocalDateTime createdAt;
@@ -30,7 +29,11 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Grade grade;
 
-    @OneToMany(mappedBy = "member")
-    private List<MealList> mealListList = new ArrayList<>();
-
+    //==생성 메서드==//
+    public static Member createMember(String memberName, Grade grade) {
+        Member member = new Member();
+        member.name = memberName;
+        member.grade = grade;
+        return member;
+    }
 }
