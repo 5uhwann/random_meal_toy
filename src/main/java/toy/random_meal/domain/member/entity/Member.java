70f39447b2a25ef98469598035e14bdeca5432c7
5uhwann <5uhwann27@gmail.com>
2023-01-07 23:40:47 +0900

    feat: Member 엔티티 컬럼 추가

--- src/main/java/toy/random_meal/domain/member/entity/Member.java
+++ src/main/java/toy/random_meal/domain/member/entity/Member.java
@@ -1,6 +1,8 @@
 package toy.random_meal.domain.member.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,7 +10,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Getter;
+import toy.random_meal.domain.meal.entity.MealList;
 
 @Entity
 @Getter
@@ -16,7 +20,7 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Column(name = "가member_id")
     private Long id;
 
     private LocalDateTime createdAt;
@@ -26,5 +30,7 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Grade grade;
 
+    @OneToMany(mappedBy = "member")
+    private List<MealList> mealListList = new ArrayList<>();
 
 }
