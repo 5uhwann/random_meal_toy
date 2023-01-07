32c345e686ec990b36ff2eb1db16abd8d98e5c92
5uhwann <5uhwann27@gmail.com>
2023-01-08 00:30:15 +0900

    refactor: MemberRepository Jpa 전환

--- src/main/java/toy/random_meal/domain/member/repository/MemberRepository.java
+++ src/main/java/toy/random_meal/domain/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package toy.random_meal.domain.member.repository;
 
+import java.util.List;
 import toy.random_meal.domain.member.entity.Member;
 
 public interface MemberRepository {
@@ -7,4 +8,6 @@ public interface MemberRepository {
     void save(Member member);
 
     Member findById(Long memberId);
+
+    List<Member> findAll();
 }
