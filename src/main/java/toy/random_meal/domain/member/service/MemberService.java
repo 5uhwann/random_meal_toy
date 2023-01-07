abfeb63e6a18e345201731e93e19da30be32e103
5uhwann <5uhwann27@gmail.com>
2023-01-08 00:32:03 +0900

    refactor: Jpa 전환에 따른 MemberService 리팩토링

--- src/main/java/toy/random_meal/domain/member/service/MemberService.java
+++ src/main/java/toy/random_meal/domain/member/service/MemberService.java
@@ -1,10 +1,13 @@
 package toy.random_meal.domain.member.service;
 
+import java.util.List;
 import toy.random_meal.domain.member.entity.Member;
 
 public interface MemberService {
 
-    void join(Member member);
+    Long join(Member member);
+
+    List<Member> findAll();
 
     Member findMember(Long memberId);
 }
--- src/main/java/toy/random_meal/domain/member/service/MemberServiceImpl.java
+++ src/main/java/toy/random_meal/domain/member/service/MemberServiceImpl.java
@@ -1,26 +1,43 @@
 package toy.random_meal.domain.member.service;
 
-import org.springframework.stereotype.Component;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import toy.random_meal.domain.member.entity.Member;
 import toy.random_meal.domain.member.repository.MemberRepository;
 
-@Component
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    @Override
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     @Override
-    public void join(Member member) {
-        memberRepository.save(member);
+    public List<Member> findAll() {
+        return memberRepository.findAll();
     }
 
     @Override
     public Member findMember(Long memberId) {
-        Member member = memberRepository.findById(memberId);
-        return member;
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        List<Member> findMembers = memberRepository.findAll();
+        for (Member findMember : findMembers) {
+            if (findMember.getName().equals(member.getName())) {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            }
+        }
     }
 }
