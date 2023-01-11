3c876e0c08994958f297d4ee5ff8abce45e165df
5uhwann <5uhwann27@gmail.com>
2023-01-11 22:37:47 +0900

    refactor: equals(), hashcode() 메서드 재정의

--- src/main/java/toy/random_meal/domain/member/entity/SelectedMeal.java
+++ src/main/java/toy/random_meal/domain/member/entity/SelectedMeal.java
@@ -1,5 +1,6 @@
 package toy.random_meal.domain.member.entity;
 
+import java.util.Objects;
 import javax.persistence.Embeddable;
 import lombok.Getter;
 
@@ -11,4 +12,21 @@ public class SelectedMeal {
 
     private String mealName;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SelectedMeal that = (SelectedMeal) o;
+        return Objects.equals(getMealListName(), that.getMealListName()) && Objects.equals(
+                getMealName(), that.getMealName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMealListName(), getMealName());
+    }
 }
