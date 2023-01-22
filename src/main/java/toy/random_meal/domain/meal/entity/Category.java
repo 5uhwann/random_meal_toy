59e782ddd161e03cf963e03b9f6542f5b43266a2
5uhwann <5uhwann27@gmail.com>
2023-01-22 23:25:19 +0900

    feat: Category 생성메서드 구현

--- src/main/java/toy/random_meal/domain/meal/entity/Category.java
+++ src/main/java/toy/random_meal/domain/meal/entity/Category.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import toy.random_meal.domain.common.BaseEntity;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
 
     @Id
@@ -18,5 +21,11 @@ public class Category extends BaseEntity {
     private Long id;
 
     private String name;
+
+    public static Category from(String name) {
+        Category category = new Category();
+        category.name = name;
+        return category;
+    }
 
 }
