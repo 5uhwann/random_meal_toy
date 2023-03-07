3772ff8e7dd91779da20887a81f4a6dcb22faeee
5uhwann <5uhwann27@gmail.com>
2023-03-07 22:29:04 +0900

    feat: 회원 엔티티 개발

---
+++ src/main/java/toy/random_meal/member/entity/Member.java
@@ -0,0 +1,28 @@
+package toy.random_meal.member.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String memberName;
+
+    @Embedded
+    private UserId userId;
+
+    private String password;
+
+
+}
---
+++ src/main/java/toy/random_meal/member/entity/UserId.java
@@ -0,0 +1,52 @@
+package toy.random_meal.member.entity;
+
+import java.util.Objects;
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserId {
+
+    private static final int MIN_LENGTH = 6;
+    private static final int MAX_LENGTH = 20;
+
+    private String userId;
+
+    private UserId(String userId) {
+        this.userId = userId;
+    }
+
+    public UserId of(String id) {
+        validateId(id);
+        return new UserId(id);
+    }
+
+    private void validateId(String id) {
+        Assert.notNull(id, "아이디는 필수입니다.");
+        if (id.length() < MIN_LENGTH || id.length() > MAX_LENGTH) {
+            throw new IllegalStateException("아이디는 6~20자 사이의 길이여야 합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserId memberId = (UserId) o;
+        return Objects.equals(getUserId(), memberId.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserId());
+    }
+}
