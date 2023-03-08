b2f9b0df8d59d4e25cd22e17795e81b1c35d0463
5uhwann <5uhwann27@gmail.com>
2023-03-08 11:07:43 +0900

    feat: 음식 엔티티 개발

---
+++ src/main/java/toy/random_meal/meal/entity/Meal.java
@@ -0,0 +1,39 @@
+package toy.random_meal.meal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import toy.random_meal.category.entity.Category;
+import toy.random_meal.mealinventory.entity.MealInventory;
+
+@Entity
+@Getter
+public class Meal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meal_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meal_inventory_id")
+    private MealInventory mealInventory;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "category_name")
+    private String name;
+
+    private int price;
+
+    private int selectedCount;
+
+}
