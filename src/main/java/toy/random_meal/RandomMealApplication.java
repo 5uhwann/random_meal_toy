3dbb698e6299e25a462d57efeb7ca1a694d2742f
5uhwann <5uhwann27@gmail.com>
2023-03-09 17:01:27 +0900

    feat: BaseEntity 설정 어노테이션 추가

--- src/main/java/toy/random_meal/RandomMealApplication.java
+++ src/main/java/toy/random_meal/RandomMealApplication.java
@@ -2,8 +2,10 @@ package toy.random_meal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RandomMealApplication {
 
 	public static void main(String[] args) {
