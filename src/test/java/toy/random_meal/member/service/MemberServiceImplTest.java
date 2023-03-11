44930a1f1b8b198c6c6c017ffe77f75980c3046a
5uhwann <5uhwann27@gmail.com>
2023-03-11 11:43:41 +0900

    test: 아이디 중복체크 결과 반환타입 변경에 따른 테스트 수정

--- src/test/java/toy/random_meal/member/service/MemberServiceImplTest.java
+++ src/test/java/toy/random_meal/member/service/MemberServiceImplTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import toy.random_meal.member.controller.dto.DuplicatedUserIdResponse;
 import toy.random_meal.member.entity.Member;
 import toy.random_meal.member.entity.UserId;
 
@@ -15,6 +16,24 @@ class MemberServiceImplTest {
     @Autowired
     private MemberService memberService;
 
+
+    @Test
+    void 아이디중복체크_성공() {
+        //given
+        Member memberA = Member.builder()
+                .name("5uhwann")
+                .userId(UserId.of("testtest"))
+                .password("1234").build();
+
+        memberService.joinMember(memberA);
+
+        //when
+        DuplicatedUserIdResponse response = memberService.checkDuplicatedUserId(UserId.of("testtesttest"));
+
+        //then
+        assertThat(response.isDuplicatedUserId()).isEqualTo(true);
+
+    }
     @Test
     void 아이디중복체크_실패() {
         //given
@@ -25,9 +44,11 @@ class MemberServiceImplTest {
 
         memberService.joinMember(memberA);
 
-        //when //then
-        assertThatThrownBy(() -> memberService.checkDuplicatedUserId(UserId.of("testtest")))
-                .isInstanceOf(IllegalArgumentException.class);
+        //when
+        DuplicatedUserIdResponse response = memberService.checkDuplicatedUserId(UserId.of("testtest"));
+
+        //then
+        assertThat(response.isDuplicatedUserId()).isEqualTo(false);
 
     }
 
