af8894d390efc4cde9a82a118aa27aabb2746255
5uhwann <5uhwann27@gmail.com>
2023-03-13 16:33:34 +0900

    refactor: 카테고리 엔티티 수정 및 정적메서드 팩터리 작성

--- src/main/java/toy/random_meal/category/entity/Category.java
+++ src/main/java/toy/random_meal/category/entity/Category.java
@@ -2,22 +2,41 @@ package toy.random_meal.category.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import toy.random_meal.common.entity.BaseEntity;
+import toy.random_meal.member.entity.Member;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cateogry_id")
+    @Column(name = "category_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @Column(name = "category_name")
     private String name;
 
+    private Category(Member member, String name) {
+        this.member = member;
+        this.name = name;
+    }
+
+    public static Category of(Member member, String categoryName) {
+        return new Category(member,categoryName);
+    }
 }
