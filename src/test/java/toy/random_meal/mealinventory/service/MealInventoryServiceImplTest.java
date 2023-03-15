14ee35a35b2124388f4791b8d8bbd33e5b78d41d
5uhwann <5uhwann27@gmail.com>
2023-03-15 22:12:41 +0900

    test: 음식목록 조회 테스트 작성

---
+++ src/test/java/toy/random_meal/mealinventory/service/MealInventoryServiceImplTest.java
@@ -0,0 +1,57 @@
+package toy.random_meal.mealinventory.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toy.random_meal.mealinventory.controller.dto.MealInventoryDto;
+import toy.random_meal.mealinventory.entity.MealInventory;
+import toy.random_meal.member.entity.Member;
+import toy.random_meal.member.entity.UserId;
+import toy.random_meal.member.service.MemberService;
+
+@SpringBootTest
+class MealInventoryServiceImplTest {
+
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MealInventoryServiceImpl mealInventoryService;
+
+    @Test
+    void 음식목록조회() {
+        //given
+        Member member = createMember();
+
+        memberService.joinMember(member);
+
+        MealInventory mealInventoryA = createMealInventory(member, "testInventoryA");
+
+        MealInventory mealInventoryB = createMealInventory(member, "testInventoryB");
+
+        mealInventoryService.createMealInventory(mealInventoryA);
+        mealInventoryService.createMealInventory(mealInventoryB);
+
+        //when
+        List<MealInventoryDto> foundMealInventory = mealInventoryService.findAllByMember(member);
+
+        //then
+        assertThat(foundMealInventory.isEmpty()).isFalse();
+    }
+
+    private static MealInventory createMealInventory(Member member, String inventoryName) {
+        return MealInventory.builder()
+                .member(member)
+                .name(inventoryName).build();
+    }
+
+    private static Member createMember() {
+        return Member.builder()
+                .name("5uhwann")
+                .userId(UserId.from("testtest"))
+                .password("1234").build();
+    }
+
+}
